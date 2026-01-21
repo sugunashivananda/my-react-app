fe716eef8e736a55fda3071e97162161abc79adf
sugunashivananda <sugunashivananda@gmail.com>
2026-01-21 11:36:26 +0530

    pushreact

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,1 @@
-import React, { useState } from 'react';
-
-const App: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
-
-  const handleClick = (): void => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <button onClick={handleClick}>Click Me</button>
-      <p>Counter: {count}</p>
-    </div>
-  );
-};
-
-export default App;
+<p>hello</p>
